c3e629ea7ed3f7708c1b1b794760dd757504b587
polo <lg550055@hotmail.com>
2022-01-17 22:31:56 -0600

    Adds error handling alert

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Alert from 'react-bootstrap/Alert';
 
 class App extends React.Component {
   constructor(props){
@@ -11,7 +12,10 @@ class App extends React.Component {
     this.state={
       city: '',
       cityInfo: {},
-      mapUrl: ''
+      mapUrl: '',
+      errorStatus: '',
+      errorData: '',
+      showAlert: false
     }
   }
 
@@ -29,12 +33,16 @@ class App extends React.Component {
       let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${this.state.city}&format=json`;
       let cityResults = await axios.get(url);
       this.setState({ cityInfo: cityResults.data[0] });
-      console.log(this.state.cityInfo);
-      let mapUrl = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_KEY}&center=${this.state.cityInfo.lat},${this.state.cityInfo.lon}&zoom=10`
+      
+      let mapUrl = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_KEY}&center=${this.state.cityInfo.lat},${this.state.cityInfo.lon}&zoom=9`
       this.setState({ mapUrl: mapUrl})
 
     } catch(error) {
-      console.log(error.response.status, error.response.data.error);
+      this.setState({
+        errorStatus: error.response.status,
+        errorData: error.response.data.error,
+        showAlert: true
+      })
     }
   }  
 
@@ -42,14 +50,15 @@ class App extends React.Component {
     return(
       <>
         <h1>Which city would you like to explore?</h1>
-        <Form style={{width: '50%', margin: 'auto'}} onSubmit={this.handleSubmit}>
-          <Form.Group>
-            <Form.Label>Enter city</Form.Label>
+        <Alert show={this.state.showAlert}>{this.state.errorStatus}, {this.state.errorData}</Alert>
+        <Form style={{width: '300px', margin: 'auto'}} onSubmit={this.handleSubmit}>
+          <Form.Group style={{margin: '2vh'}}>
+            <Form.Label>Enter a city name</Form.Label>
             <Form.Control type="text" onInput={this.handleCity}></Form.Control>
           </Form.Group>
-          <Button variant="success" type="submit">Explore!</Button>
+          <Button variant="dark" type="submit">Explore!</Button>
         </Form>
-        <Card style={{width: '50%', margin: 'auto'}}>
+        <Card style={{width: '50%', margin: '5vh auto'}}>
           <Card.Body>
             <Card.Title>{this.state.cityInfo.display_name}</Card.Title>
             <Card.Text>
@@ -58,7 +67,6 @@ class App extends React.Component {
           </Card.Body>
           <Card.Img variant="bottom" src={this.state.mapUrl} />
         </Card>        
-        <p></p>
       </>
     )
   }
